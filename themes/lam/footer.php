8718e9dac902c48f64cd2474e47170f84dda3f9f
azizulsoftsw <azizul.softsw@gmail.com>
2020-08-27 14:19:24 +0600

    frontpage

---
+++ themes/lam/footer.php
@@ -0,0 +1,45 @@
+<footer class="footer-wrp">
+  <div class="ftr-top-nav">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="ftr-nav-inr">
+            <ul class="clearfix reset-list">
+              <li class="current-menu-item"><a href="#">Home</a></li>
+              <li><a href="#">The Family</a></li>
+              <li><a href="#">Wines</a></li>
+              <li><a href="#">Olive Oil</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="ftr-btm">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="ftr-btm-inr">
+            <div class="ftr-copyright">
+              <p>© 2020 La Macinaia. All Rights Reserved.</p>
+            </div>
+            <div class="ftr-pg-link">
+              <ul class="reset-list">
+                <li><a href="#">Privacy policy</a></li>
+                <li><a href="#">Cookie Policy</a></li>
+                <li><a href="#">Sitemap</a></li>
+              </ul>
+            </div>
+            <div class="ftr-designby">
+              <a href="#">webdesign by conversal</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> 
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
