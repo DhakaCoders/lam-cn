75a7a548b0886b52a3fe9d872e3ffb2b36354800
azizulsoftsw <azizul.softsw@gmail.com>
2020-08-28 22:34:34 +0600

    az

---
+++ themes/lam/404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<section class="main-content page-404-wrp error-404-wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="page-404  text-center">
+                    <h1>404</h1>
+                    <h3>Het lijkt erop dat er op deze locatie niets is gevonden.</h3>
+                </div>
+            </div>
+        </div><!-- end of .newsarea -->
+    </div>
+</section><!-- end of main content -->
+<?php get_footer(); ?>
--- themes/lam/page.php
+++ themes/lam/page.php
@@ -1,8 +1,10 @@
 <?php 
   get_header(); 
-
+  
+  while ( have_posts() ) :
+  the_post();
   $thisID = get_the_ID();
-
+  
   $pageTitle = get_the_title($thisID);
   $custom_page_title = get_field('aangepaste_paginatitel', $thisID);
   if(!empty(str_replace(' ', '', $custom_page_title))){
@@ -28,6 +30,63 @@
   </div>
 </section>
 
+<section class="innerpage-con-wrap">
+  <div class="container-sm">
+    <div class="row">
+      <div class="col-sm-12">
+        <article class="default-page-con">
+          <?php 
+            if(have_rows('inhoud')){ 
+            while ( have_rows('inhoud') ) : the_row(); 
+            if( get_row_layout() == 'teksteditor' ){
+              $beschrijving = get_sub_field('fc_teksteditor');
+              echo '<div class="dfp-text-module clearfix">';
+                if( !empty( $beschrijving ) ) echo wpautop($beschrijving);
+              echo '</div>';    
+            }elseif( get_row_layout() == 'afbeelding_tekst' ){
+              $fc_afbeelding = get_sub_field('fc_afbeelding');
+              $imgsrc = cbv_get_image_src($fc_afbeelding, 'dfpageg1');
+              $fc_tekst = get_sub_field('fc_tekst');
+              $fc_title = get_sub_field('fctitel');
+              $positie_afbeelding = get_sub_field('positie_afbeelding');
+              $imgposcls = ( $positie_afbeelding == 'right' ) ? 'fl-dft-rgtimg-lftdes' : '';
+              echo '<div class="fl-dft-overflow-controller">';
+                if(!empty($fc_title)) printf('<h2>%s</h2>', $fc_title);
+                echo '<div class="fl-dft-lftimg-rgtdes clearfix '.$imgposcls.'">';
+                      echo '<div class="fl-dft-lftimg-rgtdes-lft mHc" style="background: url('.$imgsrc.');"></div>';
+                echo '<div class="fl-dft-lftimg-rgtdes-rgt mHc">';
+                    echo wpautop($fc_tekst);
+                echo '</div>';
+              echo '</div></div>';      
+            }elseif( get_row_layout() == 'afbeelding' ){
+              $fc_afbeelding = get_sub_field('fc_afbeelding');
+              if( !empty( $fc_afbeelding ) ){
+                printf('<div class="dfp-plate-one-img-bx">%s</div>', cbv_get_image_tag($fc_afbeelding));
+              }
+            }elseif( get_row_layout() == 'horizontal_rule' ){
+              $rheight = get_sub_field('fc_horizontal_rule');
+              printf('<div class="dfhrule clearfix" style="height: %spx;"></div>', $rheight);
+          
+            }elseif( get_row_layout() == 'gap' ){
+             $gap = get_sub_field('fc_gap');
+             printf('<div class="gap clearfix" data-value="20" data-md="20" data-sm="20" data-xs="10" data-xxs="10"></div>', $rheight);
+            }
+          
+           endwhile;
+         } else{
+            echo '<div class="default-page-con">';
+              the_content();
+            echo '</div>';
+           }
+        ?>
+        </article>
 
+      </div>
+    </div>
+  </div>
+</section>
+<?php 
+endwhile;
+?>
 <?php get_template_part('templates/google', 'map'); ?>
 <?php get_footer(); ?>
