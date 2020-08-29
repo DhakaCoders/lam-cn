5fb1f4287450c7ee1fed0719419597be7fed9c1d
azizulsoftsw <azizul.softsw@gmail.com>
2020-08-29 22:31:48 +0600

    az

--- themes/lam/page.php
+++ themes/lam/page.php
@@ -11,7 +11,7 @@
     $pageTitle = $custom_page_title;
   } 
   $standaardbanner = get_field('bannerafbeelding', $thisID);
-  if( empty($standaardbanner) ) $standaardbanner = THEME_URI.'/assets/images/about-us-bnr-page.jpg';
+  if( empty($standaardbanner) ) $standaardbanner = THEME_URI.'/assets/images/wines-pg-bnr.png';
 ?>
 <section class="page-banner">
   <div class="page-banner-controller" style="overflow: hidden;">
@@ -31,14 +31,25 @@
 </section>
 
 <section class="innerpage-con-wrap">
-  <div class="container-sm">
+  <div class="container-md">
     <div class="row">
-      <div class="col-sm-12">
+      <div class="col-md-12">
         <article class="default-page-con">
           <?php 
             if(have_rows('inhoud')){ 
             while ( have_rows('inhoud') ) : the_row(); 
-            if( get_row_layout() == 'teksteditor' ){
+            if( get_row_layout() == 'introductietekst' ){
+              $title = get_sub_field('titel');
+              $fc_editor = get_sub_field('fc_teksteditor');
+              $afbeelding = get_sub_field('afbeelding');
+              echo '<div class="dfp-promo-module clearfix">';
+                if( !empty($title) ) printf('<div><strong class="dfp-promo-module-title">%s</strong></div>', $title);
+                if( !empty($fc_editor) ) echo wpautop( $fc_editor );
+                if( !empty($afbeelding) ){
+                  echo '<div class="dfp-plate-one-img-bx">', cbv_get_image_tag($afbeelding), '</div>';
+                }
+              echo '</div>';    
+            }elseif( get_row_layout() == 'teksteditor' ){
               $beschrijving = get_sub_field('fc_teksteditor');
               echo '<div class="dfp-text-module clearfix">';
                 if( !empty( $beschrijving ) ) echo wpautop($beschrijving);
@@ -63,13 +74,33 @@
               if( !empty( $fc_afbeelding ) ){
                 printf('<div class="dfp-plate-one-img-bx">%s</div>', cbv_get_image_tag($fc_afbeelding));
               }
+            }elseif( get_row_layout() == 'galerij' ){
+              $gallery_cn = get_sub_field('afbeeldingen');
+              $lightbox = get_sub_field('lightbox');
+              $kolom = get_sub_field('kolom');
+              if( $gallery_cn ):
+              echo "<div class='gallery-wrap clearfix'><div class='gallery gallery-columns-{$kolom}'>";
+                foreach( $gallery_cn as $image ):
+                $imgsrc = cbv_get_image_src($image['ID'], 'dfpageg1');  
+                echo "<figure class='gallery-item'><div class='gallery-icon portrait'>";
+                if( $lightbox ) echo "<a data-fancybox='gallery' href='{$image['url']}'>";
+                    //echo '<div class="dfpagegalleryitem" style="background: url('.$imgsrc.');"></div>';
+                    echo wp_get_attachment_image( $image['ID'], 'dfpageg1' );
+                if( $lightbox ) echo "</a>";
+                echo "</div></figure>";
+                endforeach;
+              echo "</div></div>";
+              endif;      
+            }elseif( get_row_layout() == 'table' ){
+              $fc_table = get_sub_field('fc_table');
+              cbv_table($fc_table);
             }elseif( get_row_layout() == 'horizontal_rule' ){
               $rheight = get_sub_field('fc_horizontal_rule');
               printf('<div class="dfhrule clearfix" style="height: %spx;"></div>', $rheight);
           
             }elseif( get_row_layout() == 'gap' ){
              $gap = get_sub_field('fc_gap');
-             printf('<div class="gap clearfix" data-value="20" data-md="20" data-sm="20" data-xs="10" data-xxs="10"></div>', $rheight);
+             printf('<div class="gap-%s"></div>', $gap);
             }
           
            endwhile;
